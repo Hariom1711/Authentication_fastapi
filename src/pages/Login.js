b25a313f123b83b761b28128f42d3b5cb35f3b0e
Hariom Ojha <97445797+Hariom1711@users.noreply.github.com>
2023-05-15 14:10:42 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,96 +1,105 @@
-import React from 'react'
-import { Button, Form, Input,message } from 'antd';
-import "./Register.css"
-import {Link,useNavigate } from "react-router-dom"
+import React from 'react';
+import { Button, Form, Input, message } from 'antd';
+import './Register.css';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { isEmail, isLength } from 'validator';
+import Cookies from 'js-cookie';
+
 const Login = () => {
+  const navigate = useNavigate();
 
+  const onFinishHandler = async (values) => {
+    try {
+      const { email, password } = values;
 
-  const navigate= useNavigate()
- 
+      // Input validation using validator.js
+      const errors = {};
 
- 
-const onfinishHandler = async (values) => {
-  try {
- 
-const res = await axios.post('http://10.0.20.133:8000/login', {
-    username: values.email,
-    password: values.password,
-  } ,{
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Accept': 'application/json',
-  },
-})
-    if (res.data.access_token) {
-      localStorage.setItem("token", res.data.access_token);
-      message.success(`Login Successfully`);
-      navigate('/');
-    } else {
-      message.error("Invalid email or password");
+      if (!isEmail(email)) {
+        errors.email = 'Invalid email address';
+      }
+
+      if (!isLength(password, { min: 8 })) {
+        errors.password = 'Password must be at least 8 characters long';
+      }
+
+      if (Object.keys(errors).length > 0) {
+        // Handle validation errors, e.g., display error messages to the user
+        console.log(errors);
+        return;
+      }
+
+      const sanitizedEmail = email.replace(/[<>"]/g, ''); // Remove <, >, and " characters
+      const sanitizedPassword = password.replace(/[<>"]/g, '');
+
+      const res = await axios.post(
+        'http://10.0.20.133:8000/login',
+        {
+          username: sanitizedEmail,
+          password: sanitizedPassword,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Accept: 'application/json',
+          },
+        }
+      );
+
+      if (res.data.access_token) {
+        // Store the token in an HTTP-only cookie
+        Cookies.set('token', res.data.access_token, { httpOnly: true });
+        message.success('Login Successful');
+        navigate('/');
+      } else {
+        message.error('Invalid email or password');
+      }
+    } catch (error) {
+      console.log(error);
+      message.error('Something went wrong');
     }
-  } catch (error) {
-    console.log(error);
-    message.error('Something went wrong');
-  }
-};
-
-
+  };
 
   return (
     <>
-   
-    <div className="form-container">
-     <Form layout="vertical" onFinish={onfinishHandler} className="card p-5 ">
-       <h2 className='text-center'>Login Form </h2>
-     {/* <Form.Item
-       label="Name"
-       name="name"
-       rules={[
-         {
-           required: true,
-           message: 'Please input your username!',
-         },
-       ]}
-     >
-       <Input />
-     </Form.Item> */}
-     <Form.Item
-       label="Email"
-       name="email"
-       rules={[
-         {
-           required: true,
-           message: 'Please input your Email',
-         },
-       ]}
-     >
-       <Input />
-     </Form.Item>
-     <Form.Item
-       label="Password"
-       name="password"
-       rules={[
-         {
-           required: true,
-           message: 'Please input your password!',
-         },
-       ]}
-     >
-       <Input.Password />
-     </Form.Item>
- <Link to="/register" className='m-2'>Not a user Register here</Link>
- 
- 
-     
-     <Button type="primary" htmlType="submit">
-        Login
-       </Button>
-     </Form>
-    </div>
-    
+      <div className="form-container">
+        <Form layout="vertical" onFinish={onFinishHandler} className="card p-5">
+          <h2 className="text-center">Login Form</h2>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Email',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Link to="/register" className="m-2">
+            Not a user? Register here
+          </Link>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
